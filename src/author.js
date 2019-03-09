9f1c98f56d94971f9c77af7f2555f29af1db29a2
Vitalii Iurev <wwjurew@gmail.com>
2019-03-09 22:34:09 +0300

    add Author styles

--- src/author.js
+++ src/author.js
@@ -2,13 +2,43 @@ import React, { Component } from 'react';
 import styled from 'styled-components'
 
 const Root = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`
+
+const Avatar = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  margin-right: 10px;
+`
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Text = styled.div`
+  font-size: 12px;
+  line-height: 12px;
+  color: ${p => p.theme.mediumFontColor};
+
+  &:first-child {
+    margin-bottom: 2px;
+  }
 `
 
 class Author extends Component {
   render() {
+    const { name, avatar } = this.props.author
     return (
       <Root>
-        {this.props.author.name}
+        <Avatar src={avatar} alt={name} />
+        <TextWrapper>
+          <Text>{name}</Text>
+          <Text>von 12 Minutes</Text>
+        </TextWrapper>
       </Root>
     );
   }
--- src/item.js
+++ src/item.js
@@ -11,10 +11,11 @@ const Root = styled.article`
 
 class Item extends Component {
   render() {
-    const { comment: { author, body, title } } = this.props
+    const { comment: { author, body, title, attachments } } = this.props
+    const avatar = attachments[0].variants.profile.url // TODO: try/catch
     return (
       <Root>
-        <Author author={author} />
+        <Author author={{ ...author, avatar }} />
         <Comment title={title} text={body} />
         {/* <Bottom stats={comment.stats} /> */}
       </Root>
