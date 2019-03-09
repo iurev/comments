588fc1c4a19faf864f40aca45ec4e328c22ac176
Vitalii Iurev <wwjurew@gmail.com>
2019-03-09 21:47:31 +0300

    add reset-css only for web widget

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+import reset from 'styled-reset'
 import List from './list'
 import Load from './load'
 
+const Root = styled.div`
+  .iurev-space-comments {
+    ${reset}
+  }
+`
 
 class App extends Component {
   render() {
     return (
-      <>
+      <Root className="iurev-space-comments">
         <List />
         <Load />
-      </>
+      </Root>
     );
   }
 }
