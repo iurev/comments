27b54cfeed49715161e0a04efae4bf68b67a6d4e
Vitalii Iurev <wwjurew@gmail.com>
2019-03-09 22:03:46 +0300

    add more theme functionality

--- src/comment.js
+++ src/comment.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import Html from 'dangerously-set-inner-html'
 
 const Root = styled.div`
+  color: ${props => props.theme.lightFontColor};
 `
 
 class Item extends Component {
--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,6 @@
 export const main = {
   darkBg: "#f7f7f7",
+  darkFontColor: "#4d4d4d",
+  mediumFontColor: "#5f5f5f",
+  lightFontColor: "#a0a0a0",
 };
