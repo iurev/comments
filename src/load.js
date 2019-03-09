3fe84af2524feace5c36be979c95a19d39bdf05d
Vitalii Iurev <wwjurew@gmail.com>
2019-03-09 23:35:20 +0300

    add Load styles

--- src/load.js
+++ src/load.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import Ink from 'react-ink'
+import { ifProp } from 'styled-tools'
 
 const Root = styled.button`
+  position: relative;
+  border: 2px solid ${p => p.theme.green};
+  width: 70px;
+  height: 70px;
+  border-radius: 35px;
+  color: ${p => p.theme.green};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: transparent;
+  font-weight: 700;
+  font-size: 9px;
+  cursor: pointer;
+  outline: none;
+  transition: background ease-out 0.3s;
+  user-select: none;
+
+  &:hover {
+    background: ${p => p.theme.lightGreen};
+  }
+
+  ${ifProp('loading', css`
+    border: 2px solid ${p => p.theme.mediumFontColor};
+    color: ${p => p.theme.mediumFontColor};
+    cursor: default;
+    pointer-events: none;
+    &:hover {
+      background: transparent;
+    }
+  `)}
 `
 
 class App extends Component {
   render() {
+    const { loading, onClick } = this.props
     return (
-      <Root>Load</Root>
+      <Root
+        onClick={onClick}
+        disabled={loading}
+        loading={loading}
+      >
+        <Ink />
+        {loading ? 'Loading' : 'Load more'}
+      </Root>
     );
   }
 }
--- src/theme.js
+++ src/theme.js
@@ -4,4 +4,6 @@ export const main = {
   darkFontColor: "#4d4d4d",
   mediumFontColor: "#5f5f5f",
   lightFontColor: "#a0a0a0",
+  green: "#0adc96",
+  lightGreen: "#0adc9614"
 };
