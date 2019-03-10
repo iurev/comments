5f830c01c882f54ff0fc6095b4a9ae68e9c52a08
Vitalii Iurev <wwjurew@gmail.com>
2019-03-11 02:24:38 +0300

    add jest snapshots for Author

---
+++ src/components/__test__/Author.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+
+import Author from '../Author';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Author />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('with all attrs', () => {
+  const tree = renderer
+    .create(<Author
+      avatar="https://example.com/image.png"
+      name="John Doe"
+      time="5 mins"
+    />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('without attrs', () => {
+  const tree = renderer
+    .create(<Author />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('without avatar', () => {
+  const tree = renderer
+    .create(<Author
+      name="John Doe"
+      time="5 mins"
+    />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
